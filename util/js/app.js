2a167cb5ecd640dba43c5cf558ad397655d57cd1
Ifeanyi Chima <chimaifeanyi29@gmail.com>
2025-06-04 14:46:09 +0100

    change grid to flex

--- util/js/app.js
+++ util/js/app.js
@@ -1,4 +1,30 @@
 // portfolio
+$('#tech-carousel').owlCarousel({
+    loop:true,
+    margin:48,
+    dots:true,
+    nav: false,
+    items: 1,
+    smartSpeed: 1000,
+    autoplay:true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:true,
+    // responsive:{
+    //     0:{
+    //         items:1
+    //     },
+    //     600:{
+    //         items:2
+    //     },
+    //     1000:{
+    //         items: 3
+    //     }
+    // }
+})
+
+
+
+// portfolio
 $('#reviews-carousel').owlCarousel({
     loop:true,
     margin:48,
